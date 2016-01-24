709d7bdb56ebf52039e373de40f34f58b1ef29e6
Dhiwakar-R <dhiwakarramesh22@gmail.com>
2016-01-24 16:20:12 +0530

    Update subsets.c

--- subsets.c
+++ subsets.c
@@ -4,32 +4,36 @@ int main()
     int n,b,a[30],k,i,j,x,y;
     printf("Enter the no. of element:"); 
     scanf("%d",&n);
-    printf("Enter the elements n:");
+    printf("Enter the elements:");
     for(k=0;k<n;k++)
     scanf("%d",&a[k]);
     printf("subsets :\n");
     for(b=1;b<=n;b++)
     {
-        for(x=0;x<=n-k;x++)
-        {  
-            printf("%d\n",a[j]);
-        }
-        if(b==1)
-            printf("%d\n",a[k]);
-        else
+        for(x=0;x<=n-b;x++)
         {
-           i=x+b-1;
-           for(j=y;j<n;j++)
-           {
-               for(i=x;i<y;i++)
-               printf("%d",a[i]);
-                 
-            }
+        if(b==1)
+        	{
+				printf("%d\n",a[x]);
+        	}
+			else
+        	{
+           	i=x+b-1;
+           	for(j=i;j<n;j++)
+           		{
+               		for(y=x;y<i;y++)
+            		{
+               			printf("%d",a[y]);
+           			}
+           			printf("%d\n",a[j]);
+            	}	
+        	}
         }
      
     }
 
 }
+
 
 
 
